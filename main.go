3d61224f9b0d45eb237edb64ea8f650522046720
Max Uible <maxuible@gmail.com>
2025-02-08 06:50:59 -0600

    lots of simplifying

--- main.go
+++ main.go
@@ -7,20 +7,23 @@ import (
 	"net/http"
 )
 
-type Post struct {
-	Title string
-	Body  string
-}
-
 var templates = template.Must(template.ParseGlob("views/*.html"))
 
 var temp_posts []Post = make([]Post, 0)
+
+var temp_post_page PostPage = PostPage{
+	Posts:  temp_posts,
+	Errors: "",
+}
 
 func main() {
 
 	for i := 0; i < 10; i++ {
 		temp_posts = append(temp_posts, Post{Title: fmt.Sprint("sample Title ", i), Body: fmt.Sprint("sample Body ", i)})
 	}
+
+	temp_post_page.Posts = temp_posts
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/posts", posts)
--- posts.go
+++ posts.go
@@ -4,21 +4,49 @@ import (
 	"net/http"
 )
 
+type Post struct {
+	Title string
+	Body  string
+}
+
+type PostPage struct {
+	Posts  []Post
+	Errors string
+}
+
 func posts(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		err := templates.ExecuteTemplate(w, "posts.html", temp_posts)
+		err := templates.ExecuteTemplate(w, "posts.html", &temp_post_page)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	} else if r.Method == http.MethodPost {
-		PostCreate(r)
-		err := templates.ExecuteTemplate(w, "posts", temp_posts)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		requestErrors := PostValidateRequest(r)
+		if len(requestErrors) != 0 {
+			err := templates.ExecuteTemplate(w, "post_form", requestErrors)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		} else {
+			PostCreate(r)
+			err := templates.ExecuteTemplate(w, "posts", temp_posts)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
 		}
 	} else {
 		http.Error(w, http.ErrNoLocation.Error(), http.StatusBadGateway)
 	}
+}
+
+func PostValidateRequest(r *http.Request) string {
+	title := r.FormValue("title")
+	var requestErrors string = ""
+	if title == "" {
+		requestErrors = requestErrors + "Title Required\n"
+	}
+
+	return requestErrors
 }
 
 func PostCreate(r *http.Request) {
